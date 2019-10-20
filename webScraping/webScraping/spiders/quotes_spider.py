356461dfe7bd24ef7e7223241c156879ed4590cf
Ayden <aydenchubbic@gmail.com>
2019-10-20 00:38:04 -0700

    fixed indentation issues

--- webScraping/webScraping/spiders/quotes_spider.py
+++ webScraping/webScraping/spiders/quotes_spider.py
@@ -1,20 +1,23 @@
 import scrapy
 
-class QuotesSpider(scrapy.Spider):
-	name = "quotes" #identifies spider, unique
 
-	def start_requests(self): #returns iterable Requests
-								#indicates where spider begins crawl from
-		urls = [
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
-        	yield scrappy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response): #
-    	page = response.url.split("/")[-2]
-    	filename = 'quotes-%s.html' % page
-    	with open(filename, 'wb') as f:
-    		f.write(response.body)
-    	self.log('Saved file %s' % filename)
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+
+#schedules scrapy.Request objects returned by start_requests
+#after receiving respinse, instantiates Response objects and passes said response as argument to Parse
