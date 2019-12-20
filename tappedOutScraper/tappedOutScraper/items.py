83e6e13131ca85365d7feb816daf936a7c91947f
Ayden <aydenchubbic@gmail.com>
2019-12-19 18:50:39 -0800

    working model, must fit to tappedout

--- tappedOutScraper/tappedOutScraper/items.py
+++ tappedOutScraper/tappedOutScraper/items.py
@@ -11,4 +11,5 @@ import scrapy
 class TappedoutscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    author_name = scrapy.Field()
     pass
--- tappedOutScraper/tappedOutScraper/spiders/tappedOutSpider.py
+++ tappedOutScraper/tappedOutScraper/spiders/tappedOutSpider.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-import scrapy
 
+#to run:
+#cd into tappedOutScaper directory and run
+#scrapy crawl tappedOutSpider -o spiderOutput.json
+import scrapy
+from ..items import TappedoutscraperItem
 
 class TappedoutspiderSpider(scrapy.Spider):
-    name = 'tappedOutSpider'
-    allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+	name = 'tappedOutSpider'
+	start_urls = ['https://www.amazon.com/b?ie=UTF8&node=17276797011']
 
-    def parse(self, response):
-        pass
+	def parse(self, response):
+		#parenthesis indicate it is a class object
+		items = TappedoutscraperItem()
+		my_author = response.css('.s-access-title::text').extract()
+		items['author_name'] = my_author
+		yield items
