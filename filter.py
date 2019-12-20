f431ea700e3d18c1617e440406d2c425dcfb9a16
Ayden <aydenchubbic@gmail.com>
2019-12-19 21:05:15 -0800

    now strips whitespace, result; list of card names. TODO; remove redundant tokens

--- filter.py
+++ filter.py
@@ -1,65 +1,80 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+# from flask import Flask, render_template, url_for, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content']
+#         new_task = Todo(content=task_content)
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue adding your task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
+#     else:
+#         tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template('index.html', tasks=tasks)
 
-    if request.method == 'POST':
-        task.content = request.form['content']
 
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
 
-    else:
-        return render_template('update.html', task=task)
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task'
 
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+
+#     if request.method == 'POST':
+#         task.content = request.form['content']
+
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
+
+#     else:
+#         return render_template('update.html', task=task)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import json
+
+def smooth(cardNames):
+    for card in range(len(cardNames)):
+        cardNames[card] = cardNames[card].strip()
+    return cardNames
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    file = open('./tappedOutScraper/spiderOutput.json')
+    data = json.load(file)
+    for dictionary in data:
+        cardNames = dictionary.get("card_name")
+        cardNames = smooth(cardNames)
+        for card in cardNames:
+            print(card)
