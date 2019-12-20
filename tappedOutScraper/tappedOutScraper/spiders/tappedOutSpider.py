c0d2345e2c3cee8d6f11b46219c10f8f4ad34a74
Ayden <aydenchubbic@gmail.com>
2019-12-19 19:27:41 -0800

    working tappedOut scraper model

--- tappedOutScraper/tappedOutScraper/spiders/tappedOutSpider.py
+++ tappedOutScraper/tappedOutScraper/spiders/tappedOutSpider.py
@@ -8,11 +8,14 @@ from ..items import TappedoutscraperItem
 
 class TappedoutspiderSpider(scrapy.Spider):
 	name = 'tappedOutSpider'
-	start_urls = ['https://www.amazon.com/b?ie=UTF8&node=17276797011']
+	# start_urls = ['https://www.amazon.com/b?ie=UTF8&node=17276797011']
+	start_urls = ['http://tappedout.net/mtg-decks/evangelion-4/']
 
 	def parse(self, response):
 		#parenthesis indicate it is a class object
 		items = TappedoutscraperItem()
-		my_author = response.css('.s-access-title::text').extract()
-		items['author_name'] = my_author
+		# my_author = response.css('.s-access-title::text').extract()
+		# items['author_name'] = my_author
+		my_name = response.css('.card-hover::text').extract()
+		items['card_name'] = my_name
 		yield items
